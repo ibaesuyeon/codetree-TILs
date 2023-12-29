f24aaef609c6208ec1629164d1624d26f8f186aa
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2023-12-30 01:52:38 +0900

    Title: 중앙값 구하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231230/중앙값 구하기/find-the-median.java
+++ 231230/중앙값 구하기/find-the-median.java
@@ -6,11 +6,13 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         int med = 0;
-        if ( a < b) {
-            if ( b < c) med = b;
-            else med = c;
-        } else if ( b < a){
-            if ( b < c) med = a;
+        if ( a <  b) {
+            if ( b < c ) med = b;
+            else if ( a < c) med = c;
+            else med = a; 
+        } else {
+            if (a < c) med = c;
+            else if ( b < c) med = c;
             else med = b;
         }
         System.out.println(med);
