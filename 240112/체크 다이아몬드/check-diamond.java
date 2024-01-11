12f8122a65f0ed2c643aa64846dc7fcce0c3bc16
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2024-01-12 05:03:23 +0900

    Title: 체크 다이아몬드, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240112/체크 다이아몬드/check-diamond.java
+++ 240112/체크 다이아몬드/check-diamond.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int space = n-1;
-        for(int i = 1; i<=(2*n-3); i++){
+        int space = 2*n-6;
+        for(int i = 1; i<=(2*n-5); i++){
             for(int j = space; j>0; j--){
                 System.out.print(" ");
             }
@@ -16,8 +16,8 @@ public class Main {
 
         }
         space = 1;
-        for(int i = (2*n-4); i>0; i--) {
-            for(int j = space; j>0; j--){
+        for(int i = 2*n-6; i>0; i--) {
+            for(int j = 0; j<space; j++){
                 System.out.print(" ");
             }
             for(int k = 0; k<i; k++) {
